2b6c2c6d87d0294233945b67e2e894f5c7a74166
yrda2335-commits <yrda2335@gmail.com>
2026-09-04 20:01:15 +0300

    Remove assessment files and dependencies related to PDF parsing

--- assessment_extract.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require 'vendor/autoload.php';
-
-$parser = new \Smalot\PdfParser\Parser();
-$pdf = $parser->parseFile('C:/Users/kh/Desktop/Laravel_Practical_Assessment_Task.pdf');
-file_put_contents('assessment.txt', $pdf->getText());
