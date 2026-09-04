c5e3954994d33225323aeb5e404ec6776379238b
yrda2335-commits <yrda2335@gmail.com>
2026-09-04 18:24:55 +0300

    Add fillable attributes and relationships for Department model

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Department extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function employees(): HasMany
+    {
+        return $this->hasMany(Employee::class);
+    }
+
+    public function projects(): HasMany
+    {
+        return $this->hasMany(Project::class);
+    }
 }
