f06d47549d76fed0cd1c4a09dd4defa2910a5cc4
yrda2335-commits <yrda2335@gmail.com>
2026-09-04 18:33:19 +0300

    Add factory classes for Department, Employee, Profile, Project, Skill, Task, and Training models

---
+++ database/factories/TrainingFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Training;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Training>
+ */
+class TrainingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
